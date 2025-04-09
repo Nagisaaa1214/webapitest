f292202a7909dc40559205b3ce0186106b153219
anson <ansonlcy1214@gmail.com>
2025-04-09 10:17:51 +0800

    L7

--- index.ts
+++ index.ts
@@ -2,40 +2,44 @@ import Koa from "koa";
 import Router, { RouterContext } from "koa-router";
 import logger from "koa-logger";
 import json from "koa-json";
+import bodyParser from "koa-bodyparser";
+import passport from "koa-passport";
 import { router as articles } from "./routes/articles";
+import { router as special } from "./routes/specials";
 
 
 const app: Koa = new Koa();
-//const router: Router = new Router();
-
-/*const welcomeAPI = async (ctx: RouterContext, next:any) => {
-  ctx.body = {message: "Welcome to the blog API!"};
+const welcomeAPI = async (ctx: RouterContext, next: any) => {
+  ctx.body = {
+    msg: "Welcome to the blog API",
+  };
   await next();
-}
+};
+const router: Router = new Router();
 
-router.get('/api/v1', welcomeAPI);*/
-// For Document:
+//router.get('/api/v1', welcomeAPI);
 
-app.use(logger());
 app.use(json());
+app.use(logger());
+app.use(bodyParser());
+app.use(router.routes());
+//app.use(articles.routes());
+app.use(passport.initialize());
+app.use(special.middleware());
 app.use(articles.middleware());
 
-
-
-
 app.use(async (ctx: RouterContext, next: any) => {
-    try {
-        await next();
-        console.log(ctx.status)
-        if (ctx.status === 404) {
-            ctx.body = { err: "Resource not found" };
-        }
-    } catch (err: any) {
-        ctx.body = { err: err };
+  try {
+    await next();
+    if (ctx.status === 404) {
+      ctx.status = 404;
+      ctx.body = { err: "No such endpoint existed" };
     }
-
+  } catch (err: any) {
+    ctx.body = { err: err };
+  }
 });
 
 app.listen(10888, () => {
-    console.log("Koa Started");
+  console.log("Koa Started");
 });
